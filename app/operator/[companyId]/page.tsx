9943307cdd1336fd54df349a1dc8a8ad9be148ee
Adebola Akeredolu <debola31@gmail.com>
2026-03-11 17:38:38 -0700

    operator redirect

---
+++ app/operator/[companyId]/page.tsx
@@ -0,0 +1,10 @@
+import { redirect } from 'next/navigation';
+
+export default async function OperatorRoot({
+  params,
+}: {
+  params: Promise<{ companyId: string }>;
+}) {
+  const { companyId } = await params;
+  redirect(`/operator/${companyId}/jobs`);
+}
